1a8ff490bd5d7ecac73ca02bf9026775da196b70
Fabrizio Furnari <fab.furnari@gmail.com>
2014-11-19 23:02:36 +0100

    first model stub

---
+++ daphnis/model.py
@@ -0,0 +1,35 @@
+from daphnis import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    registered_on = db.Column(db.DateTime)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
