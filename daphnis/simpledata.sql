f4651b3346034863242e4418284d765d879162a7
Fabrizio Furnari <fab.furnari@gmail.com>
2014-12-03 16:56:05 +0100

    start using real data

--- daphnis/simpledata.sql
+++ daphnis/simpledata.sql
@@ -6,9 +6,9 @@ INSERT INTO "tag" VALUES(3,'cinema');
 
 INSERT INTO "user" VALUES(1,'fixed','test@test.com','pbkdf2:sha1:1000$IpIDYJGd$68ab7b378452581d4cbf5ed1f5f6646a9a107335',NULL,NULL);
 
-INSERT INTO "feed" VALUES(1,'test feed','http://www.google.com',1);
-INSERT INTO "feed" VALUES(2,'another test','http://stocazzo.com',1);
-INSERT INTO "feed" VALUES(3,'ancora uno ','http://www.none.com',1);
+INSERT INTO "feed" VALUES(1,'Test feed','http://www.feedforall.com/sample.xml',1);
+INSERT INTO "feed" VALUES(2,'Another test','http://www.feedforall.com/sample-feed.xml',1);
+INSERT INTO "feed" VALUES(3,'Invalid feed','http://www.none.com',1);
 
 INSERT INTO "tagmap" VALUES(1,1);
 INSERT INTO "tagmap" VALUES(2,2);
